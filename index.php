94a406b46159cd93373a67b2364441e341d87e7f
Prabhakar Gupta <prabhakargupta267@gmail.com>
2015-08-28 22:29:59 +0530

    UI Improvements

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php require 'inc/header.inc.php';?>
+<?php
+    require 'inc/header.inc.php';
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,95 +9,83 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Prabhakar Gupta">
-	<link rel="shortcut icon" href="../../img/icon.png">
+
     <title>Image Extractor</title>
+
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
-    <style type="text/css">
-    .header-image{
-        background-color: rgb(239, 250, 141);
-        background-image: url('bg.png');
-        padding: 20px;
-        text-align: center;
-        font-family: Futura, Century Gothic, AppleGothic, sans-serif;
-    }
-	.other-text{
-		font-family: Futura, Century Gothic, AppleGothic, sans-serif;
-		font-size : 16px;
-		margin: 10px;
-		text-align: center;
-	}
-    </style>
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
-<body style="background-color:rgb(249, 249, 249);">
+<body>
     <header class="header-image">
         <div class="container">
             <h1><strong>Image Extractor</strong></h1>
-            <h2><strong>Enter any URL and get all the images on the page</strong></h2>
+            <h3>Enter any URL and get all the images on the page</h3>
         </div>
     </header>
     <hr>
-    <form class="form-inline text-center" action="<?php echo $script_name;?>" method="POST">
-        
-          <label class="control-label" for="inputSuccess1">URL : </label>
-          <input type="text" class="form-control" name="url" required>
-          <button type="submit" class="btn btn-success">Extract</button>
-    </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <form class="form-horizontal form-main" method="GET">
+                      <!-- <label class="control-label" for="inputSuccess1">URL : </label> -->
+                      <input type="text" class="form-control" name="url" placeholder="Enter the URL from where images are to be extracted" required>
+                      <br>
+                      <button type="submit" class="btn btn-lg btn-success">Extract</button>
+                </form>
+            </div>
+        </div>
+    </div>
 <?php
-    function isValidURL($url){
-        return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url);
-    }
+function isValidURL($url){
+    return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url);
+}
 
-    if(isset($_POST['url'])){
-        $url = $_POST['url'];
-        
+if(isset($_GET['url'])){
+    $url = $_GET['url'];
+    
+    $parts = explode('/', $url);
 
-        $parts = explode('/', $url);
-        $flag = ($parts[0]=='http:' || $parts[0]=='https:')?1:0;
+    $flag = ($parts[0] == 'http:' || $parts[0] == 'https:') ? true : false;
 
-        if(!@$flag)
-            $url = 'http://'.$url;
+    if($flag == false)
+        $url = 'http://'.$url;
 
-        if(!isValidURL($url)){
-            echo '<div class="other-text">Invalid URL!</div>';
-			die();
-		}
+    if(!isValidURL($url)){
+        echo '<div class="other-text">Invalid URL!</div>';
+		die;
+	}
 
-        if(substr($url, strlen($url)-1) == '/')
-            $url = rtrim($url,"/");
+    if(substr($url, strlen($url)-1) == '/')
+        $url = rtrim($url, "/");
 
-        $filename = 'websites.txt';
-        $handle = fopen($filename, 'a');
-        fwrite($handle, $url."\n");
-        fclose($handle);
-            
-        $parts = explode('/', $url);
-        $Root = $parts[0].'//'.$parts[2];
-        $html = @file_get_contents($url);
-        
-        if(preg_match_all('/<img[^>]+>/i',$html, $result)){
-            echo '<div class="other-text"><strong>URL Searched</strong> : <a href="'.$url.'" target="_blank">'.$url.'</a><br><strong>Parent Domain</strong> : <a href="'.$Root.'" target="_blank">'.$Root.'</a><br></div>';
-            foreach ($result[0] as $key) {
-                preg_match('/src="([^"]+)/i',$key, $src_key);
-                for($i=0;$i<count($src_key);$i+=2){
-                    $src = $src_key[1];
-                    if(!preg_match("/http:/", $src) && !preg_match("/https:/", $src)){
-                        if($src[0]=='/' && $src[1]=='/')
-                            $src = 'http:'.$src;
-                        elseif($src[0]=='/')
-                            $src = $Root.$src;
-                        else
-                            $src = $Root.'/'.$src;
-                    }
-                    echo '<a href="'.$src.'"><img src="'.$src.'" width="250" style="margin:20px"></a>'."\n";
+    $parts = explode('/', $url);
+    $Root = $parts[0].'//'.$parts[2];
+    $html = @file_get_contents($url);
+    
+    if(preg_match_all('/<img[^>]+>/i',$html, $result)){
+        echo '<div class="other-text"><strong>URL Searched</strong> : <a href="'.$url.'" target="_blank">'.$url.'</a><br><strong>Parent Domain</strong> : <a href="'.$Root.'" target="_blank">'.$Root.'</a><br></div>';
+        foreach ($result[0] as $key) {
+            preg_match('/src="([^"]+)/i',$key, $src_key);
+            for($i=0;$i<count($src_key);$i+=2){
+                $src = $src_key[1];
+                if(!preg_match("/http:/", $src) && !preg_match("/https:/", $src)){
+                    if($src[0]=='/' && $src[1]=='/')
+                        $src = 'http:'.$src;
+                    elseif($src[0]=='/')
+                        $src = $Root.$src;
+                    else
+                        $src = $Root.'/'.$src;
                 }
+                echo '<a href="'.$src.'"><img src="'.$src.'" width="250" style="margin:20px"></a>'."\n";
             }
-        } else {
-			echo '<div class="other-text"><b>No Image Found at your Given Location</b></div>';
         }
     } else {
-        echo '<div class="other-text">Welcome :)</div>';
+		echo '<div class="other-text"><b>No Image Found at your Given Location</b></div>';
     }
+} else {
+    echo '<div class="other-text">Welcome :)</div>';
+}
 ?>
 
 </body>
